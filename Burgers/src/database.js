d435b9d9606f4a922701e78a0189093e8a8aa7ac
Bernardo Quintino <b.quintinoguzman@gmail.com>
2019-11-27 23:33:11 -0600

    modulos

---
+++ Burgers/src/database.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/burgers-db',{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useFindAndModify:false
+}).then(db=> console.log('DB is connected')).catch(err=>console.log(err));
