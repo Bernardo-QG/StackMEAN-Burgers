f131ff8799892349c912704cf6de8b83790f097f
Bernardo Quintino <b.quintinoguzman@gmail.com>
2019-11-29 19:03:00 -0600

    view pdidos

--- Burgers/src/routes/pedidos.js
+++ Burgers/src/routes/pedidos.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router=express.Router();
 
+router.get('/pedidos',(req,res)=>{
+    res.render('pedidos/order');
+});
+router.get('/pedidos/add',(req,res)=>{
+    res.render('pedidos/new-order');
+});
+router.get('/pedidos/edit',(req,res)=>{
+    res.render('pedidos/edit-order');
+});
+
 module.exports=router;
