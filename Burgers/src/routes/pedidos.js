c6324f5b51d83b6417fa0a38a8b949dac05b1485
Bernardo Quintino <b.quintinoguzman@gmail.com>
2019-12-03 11:22:53 -0600

    .

--- Burgers/src/routes/pedidos.js
+++ Burgers/src/routes/pedidos.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const router=express.Router();
+const Alimento = require('../models/Alimento');
 
-router.get('/pedidos',(req,res)=>{
+router.get('/pedidos',(req,res)=>{    
     res.render('pedidos/order');
 });
+
+
 router.get('/pedidos/add',(req,res)=>{
-    res.render('pedidos/new-order');
+    res.render('pedidos/new-order');    
 });
+
+router.get('/pedidos/add/alimentos',async(req,res)=>{
+    const alimentos = await Alimento.find().sort({date: 'desc'});
+    res.render('pedidos/order-alimentos',{alimentos});    
+});
+
+
+
 router.get('/pedidos/edit',(req,res)=>{
     res.render('pedidos/edit-order');
 });
 
+
+
 module.exports=router;
+
+
+
